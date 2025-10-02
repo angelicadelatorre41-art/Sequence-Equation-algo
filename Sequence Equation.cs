e93b992ad3634a47f2e97485352cc8376f188a8e
angelicadelatorre41-art <angelicadelatorre41@gmail.com>
2025-10-02 10:56:40 +0800

    Sequence Equation.cs

---
+++ Sequence Equation.cs
@@ -0,0 +1,66 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'permutationEquation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY p as parameter.
+     */
+
+    public static List<int> permutationEquation(List<int> p)
+    {
+         int n = p.Count;
+        List<int> result = new List<int>(n);
+
+        int[] indexMap = new int[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            indexMap[p[i]] = i + 1;
+        }
+
+        for (int y = 1; y <= n; y++)
+        {
+         
+            int x = indexMap[y];
+          
+            result.Add(indexMap[x]); 
+        }
+
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> p = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(pTemp => Convert.ToInt32(pTemp)).ToList();
+
+        List<int> result = Result.permutationEquation(p);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
